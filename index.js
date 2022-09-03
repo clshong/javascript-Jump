5368c69e0beafe72ccc921b7cefdde9a5b89aba1
goddits <qcsjin8@gmail.com>
2022-09-03 10:33:15 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-let num = -7;
+let num = 80;
 
 switch (true) {
   case num < 0:
-    alert('num小于0');
+    console.log('num小于0');
     break;
   case num >= 0 && num <= 10:
-    alert('num 在0-10之间');
+    console.log('num 在0-10之间');
     break;
   case num > 10 && num <= 20:
-    alert('num 在0-20之间');
+    console.log('num 在0-20之间');
     break;
   default: {
-    alert('num 大于20');
+    console.log('num 大于20');
   }
 }
+
+function sayHi(name, message) {
+  // 模板字符串
+  console.log(`hello${name},${message}`);
+  // 字符串拼接
+  console.log('hello' + name + ',' + message);
+}
+sayHi('charles', 'wath are you doing');
+
+function diff(num1, num2) {
+  if (num1 < num2) {
+    return num2 - num1;
+  } else {
+    return num1 - num2;
+  }
+}
+console.log(diff(10, 5));
+
+
